7cb2597d1cc6936f664fb0442a92e0204fea1d27
fillatc <cyril@fillatre.eu>
2022-06-02 12:38:35 +0000

    continue working after test

---
+++ src/main/java/org/example/utils/RandomSingleton.java
@@ -0,0 +1,25 @@
+package org.example.utils;
+
+import java.util.Random;
+
+public class RandomSingleton {
+
+    private static RandomSingleton instance = null;
+
+    private Random random;
+
+    public static RandomSingleton getInstance() {
+        if (instance == null) {
+            instance = new RandomSingleton();
+        }
+        return instance;
+    }
+
+    public int getNextRandom(int min, int max) {
+        return this.random.nextInt(max - min) + min;
+    }
+
+    private RandomSingleton() {
+        this.random = new Random();
+    }
+}
